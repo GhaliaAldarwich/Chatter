1b75160e5c1db4e9fc6308c1910a41a311e3ca7f
GhaliaAldarwich <ghaliazaher7@gmail.com>
2025-05-27 02:09:50 +0300

    Fixed layout and styling issues for the home page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Automatically run ESLint on production builds
+    dirs: ['src', 'pages'],  // Specify the directories to lint
+    ignoreDuringBuilds: false,  // Fail the build if linting errors are found
+  },
+  // other settings
+}
 
-export default nextConfig;
+export default config;
